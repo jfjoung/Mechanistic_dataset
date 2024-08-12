91039ac89835e9e7c41202a8d697dd745809dc24
jfjoung <jjoung@mit.edu>
2024-08-12 14:52:10 -0400

    Fix Suzuki reactions

--- scripts/analysis_setting.py
+++ scripts/analysis_setting.py
@@ -1,7 +1,7 @@
 class Args:
     def __init__(self):
         self.data = './results/USPTO.pickle'  # Path to the file where results were saved.
-        self.template_analysis = False
+        self.template_analysis = True
         self.ERS_analysis = True
         self.mol_analysis = True
         self.process = 32  # Number of processors to use in multiprocessing.
